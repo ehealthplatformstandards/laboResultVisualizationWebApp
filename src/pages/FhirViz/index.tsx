0b2f7c00460bd897a4009576ec609affc67289a7
Antoine Duchâteau <ad@taktik.com>
2021-11-26 12:11:41 +0100

    Support for validation of JSONs

--- src/pages/FhirViz/index.tsx
+++ src/pages/FhirViz/index.tsx
@@ -4,11 +4,12 @@ import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useDispatch } from 'react-redux'
 import 'antd/dist/antd.css'
-import { Upload, Tabs, message } from 'antd'
+import { Upload, Tabs, message, Switch, Button, Modal } from 'antd'
 import { InboxOutlined } from '@ant-design/icons'
 import { RouteComponentProps } from 'react-router'
 
 import { v4 as uuid } from 'uuid'
+import { TabsType } from 'antd/es/tabs'
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface FhirVizRouteParams {}
@@ -20,27 +21,32 @@ function FhirViz({ match }: RouteComponentProps<FhirVizRouteParams>): JSX.Elemen
   const [fileList, setFileList] = useState([])
   const [tabsList, setTabsList] = useState([])
   const [activeKey, setActiveKey] = useState(null as string)
+  const [validate, setValidate] = useState(true)
+  const [displayedValidation, setDisplayedValidation] = useState(null as string)
 
-  const onChange = (targetKey) => setActiveKey(targetKey)
-  const remove = (targetKey) => {
-    const idx = tabsList.map((t) => t.key).indexOf(targetKey)
-    if (idx < 0) return
+  const tabsProps = {
+    hideAdd: true,
+    type: 'editable-card' as TabsType,
+    activeKey: activeKey,
+    onChange: (targetKey: string) => setActiveKey(targetKey),
+    onEdit: (targetKey: any, action: 'remove' | 'add') => {
+      if (action === 'remove') {
+        const idx = tabsList.map((t) => t.key).indexOf(targetKey)
+        if (idx < 0) return
 
-    const newTabs = tabsList.filter((pane) => pane.key !== targetKey)
+        const newTabs = tabsList.filter((pane) => pane.key !== targetKey)
 
-    setTabsList(newTabs)
-    setActiveKey(!newTabs.length ? null : idx > newTabs.length - 1 ? newTabs[newTabs.length - 1].key : newTabs[idx].key)
+        setTabsList(newTabs)
+        setActiveKey(!newTabs.length ? null : idx > newTabs.length - 1 ? newTabs[newTabs.length - 1].key : newTabs[idx].key)
+      }
+    },
   }
 
-  const onEdit = (targetKey, action) => {
-    action === 'remove' && remove(targetKey)
-  }
-
-  const props = {
+  const draggerProps = {
     name: 'file',
     multiple: true,
-    action: 'https://fhirviz.icure.dev/rest/fhir/r4/viz/div/single',
-    onChange(info) {
+    action: validate ? 'https://fhirviz.icure.dev/rest/fhir/r4/viz/div/single/validate' : 'https://fhirviz.icure.dev/rest/fhir/r4/viz/div/single',
+    onChange: async (info: any) => {
       const { status, name, response, uid } = info.file
       setFileList(info.fileList ?? [])
 
@@ -48,15 +54,24 @@ function FhirViz({ match }: RouteComponentProps<FhirVizRouteParams>): JSX.Elemen
         message.success(`${name} file uploaded successfully.`)
         if (response) {
           const key = uuid()
-          setTabsList(tabsList.concat({ key: key, title: info.file.name, preview: response.toString() }).filter((_, idx, a) => a.length - idx <= 36))
+          setTabsList(
+            tabsList
+              .concat(
+                validate
+                  ? { key: key, title: info.file.name, preview: response.html, errors: response.errors, validation: response.validation }
+                  : { key: key, title: info.file.name, preview: response.toString() },
+              )
+              .filter((_, idx, a) => a.length - idx <= 36),
+          )
           setActiveKey(key)
-          setFileList(info.fileList.filter((x) => x.uid !== uid))
+          setFileList(fileList.filter((x: any) => x.uid !== uid))
         }
       } else if (status === 'error') {
         message.error(`${info.file.name} file upload failed.`)
+        setFileList(fileList.filter((x: any) => x.uid !== uid))
       }
     },
-    onDrop(e) {
+    onDrop(e: any) {
       console.log('Dropped files', e.dataTransfer.files)
     },
   }
@@ -66,7 +81,10 @@ function FhirViz({ match }: RouteComponentProps<FhirVizRouteParams>): JSX.Elemen
 
   return (
     <>
-      <Dragger {...props} fileList={fileList}>
+      <p>
+        Perform validation on uploaded files: <Switch checked={validate} onChange={(checked) => setValidate(checked)} />
+      </p>
+      <Dragger {...draggerProps} fileList={fileList}>
         <p className="ant-upload-drag-icon">
           <InboxOutlined />
         </p>
@@ -74,13 +92,22 @@ function FhirViz({ match }: RouteComponentProps<FhirVizRouteParams>): JSX.Elemen
         <p className="ant-upload-hint">{t('FhirViz.singleOrBulkSupport', 'Upload one or several FHIR files to display a rendered document')}</p>
       </Dragger>
 
-      <Tabs hideAdd type="editable-card" onChange={onChange} activeKey={activeKey} onEdit={onEdit}>
+      <Tabs {...tabsProps}>
         {tabsList.map((t, idx) => (
           <TabPane key={t.key} tab={t.title} closable={true}>
+            {t.validation && (
+              <Button className={['validationButton', t.errors ? 'red' : 'green'].join(' ')} type="primary" shape="round" onClick={() => setDisplayedValidation(t.validation)}>
+                {t.errors} {t.errors > 1 ? 'errors' : 'error'}
+              </Button>
+            )}
             <div className="preview" dangerouslySetInnerHTML={{ __html: t.preview }} />
           </TabPane>
         ))}
       </Tabs>
+
+      <Modal width={'1280px'} footer={null} title={null} visible={!!displayedValidation} onOk={() => setDisplayedValidation(null)} onCancel={() => setDisplayedValidation(null)}>
+        <iframe style={{ border: 0, outline: 'none', height: '860px', width: '100%' }} srcDoc={displayedValidation} />
+      </Modal>
     </>
   )
 }
